889aebd91100a05fdd9c0a134b513becad3be5ee
Ubuntu <ubuntu@ov-ff7805.infomaniak.ch>
2025-03-26 16:19:17 +0000

    AUTOCOMMIT 2025-03-26 16:19:17 UTC

--- dashboard.py
+++ dashboard.py
@@ -125,4 +125,4 @@ def update_graph(selected_user, selected_muscle, selected_exercise):
     return fig
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8055)
+    app.run(debug=True, host='0.0.0.0', port=8080)
